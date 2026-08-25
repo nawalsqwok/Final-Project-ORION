f200062fb955b03691f621e27faf43555e12156c
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-08-25 12:41:31 +0700

    fix MapLibre worker for production

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,9 +1,14 @@
 import {
     Map,
     NavigationControl,
+    setWorkerUrl,
 } from "maplibre-gl";
 
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
+
 import "maplibre-gl/dist/maplibre-gl.css";
+
+setWorkerUrl(workerUrl);
 
 import { getOrionDistricts } from "../api/orion-data.js";
 import { createLocationPopup } from "../components/location-popup.js";
